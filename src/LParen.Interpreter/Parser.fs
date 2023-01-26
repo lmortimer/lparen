7e4e55c8c8236c0d14afa4423ee8e7dc854b2c5f
Logan <logan@isthisit.nz>
2023-01-26 14:47:16 +1100

    Replace FPParsec with a native F# implementation for portability. eyeing fable compilataion

--- src/LParen.Interpreter/Parser.fs
+++ src/LParen.Interpreter/Parser.fs
@@ -1,8 +1,7 @@
 module LParen.Interpreter.Parser
 
 open LParen.Interpreter.Common
-open FParsec
-
+open LParen.Interpreter.Parsec
 // customising error messages https://www.quanttec.com/fparsec/users-guide/customizing-error-messages.html
 
 // let parseString =
@@ -13,7 +12,7 @@ let parseBoolean =
     (pstring "true" >>% Atom.Boolean true) <|>
     (pstring "false" >>% Atom.Boolean false) 
 
-let atomValue, atomValueRef = createParserForwardedToRef<Atom, unit>()
+let atomValue, atomValueRef = createParserForwardedToRef()
 
 let parseSymbol =
     (pstring "+" >>% Atom.Symbol "+") <|>
@@ -25,7 +24,7 @@ let parseInteger = pint32 |>> Atom.Integer
 
 let parseList = 
     skipChar '(' >>. spaces >>. 
-    (attempt (sepBy atomValue spaces1) <|> sepEndBy1 atomValue spaces1)
+    (FParsecCompat.attempt (sepBy atomValue spaces1) <|> sepEndBy1 atomValue spaces1)
     .>> spaces .>> skipChar ')' 
     |>> Atom.List
     
@@ -48,13 +47,13 @@ let parseManyExpressions = many parseSingleExpression
 // for general use of the language
 let lParenParser str =
     
-    match run parseManyExpressions str with
-    | Success (h, _, _) -> Microsoft.FSharp.Core.Ok h
-    | Failure (errorMsg, _, _) -> Microsoft.FSharp.Core.Error errorMsg
+    match runString parseManyExpressions () str with
+    | Ok (h, _, _) -> Microsoft.FSharp.Core.Ok h
+    | Error errorValue -> Error (errorValue.ToString())
 
 // used in testing
 let singleExpressionParser str =
     
-    match run parseSingleExpression str with
-    | Success (h, _, _) -> Microsoft.FSharp.Core.Ok h
-    | Failure (errorMsg, _, _) -> Microsoft.FSharp.Core.Error errorMsg
+    match runString parseSingleExpression () str with
+    | Ok (h, _, _) -> Microsoft.FSharp.Core.Ok h
+    | Error errorValue -> Error (errorValue.ToString())
